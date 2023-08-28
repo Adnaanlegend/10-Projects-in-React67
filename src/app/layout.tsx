84cfc3b32eb401bf9cfc49c0192a9fae334341e9
Adnaan Hussain <adnaan137@gmail.com>
2023-08-28 11:32:11 +0530

    Adding in the Favorites Page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Button } from "@/components/ui/button";
+import { Heart } from "@/components/ui/icons/heart";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +22,9 @@ function SideMenu() {
             Manage
           </h2>
           <div className="space-y-1">
-            <Button variant="secondary" className="w-full gap-2 justify-start">
+            <Button asChild 
+            variant="secondary" className="w-full gap-2 justify-start">
+              <Link href="/gallery">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -36,8 +40,9 @@ function SideMenu() {
                 />
               </svg>
               Gallery
+              </Link>
             </Button>
-            <Button variant="ghost" className="w-full gap-2 justify-start">
+            {/* <Button variant="ghost" className="w-full gap-2 justify-start">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -53,23 +58,13 @@ function SideMenu() {
                 />
               </svg>
               Albums
-            </Button>
-            <Button variant="ghost" className="w-full gap-2 justify-start">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-                />
-              </svg>
+            </Button> */}
+            <Button asChild 
+            variant="ghost" className="w-full gap-2 justify-start">
+            <Link href="/favorites" >
+            <Heart />
               Favorites
+              </Link>
             </Button>
           </div>
         </div>
